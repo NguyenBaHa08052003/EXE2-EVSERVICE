52875058c75573491f8d2fc93c00ac59e8b563ec
NguyenBaHa08052003 <nguyenbaha0805@gmail.com>
2025-10-30 22:55:26 +0700

    fix

--- WDP-BE-EXE/DB/db.js
+++ WDP-BE-EXE/DB/db.js
@@ -3,7 +3,7 @@ var connect_uri = process.env.MONGO_URI
 // var uri = process.env.MONGO_URI
 const connectDB = async (req, res) => {
     try {
-        const connect = await mongoose.connect(DB_CONNECT);
+        const connect = await mongoose.connect(process.env.DB_CONNECT);
         console.log("Connect Done")
     } catch (error) {
         console.error(' MongoDB connection error:', error.message);
