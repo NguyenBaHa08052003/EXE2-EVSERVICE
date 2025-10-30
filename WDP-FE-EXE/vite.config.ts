4c67a7ca205f5f93aff926aec1b2357a13499f27
NguyenBaHa08052003 <nguyenbaha0805@gmail.com>
2025-10-30 23:29:37 +0700

    fix call url be

--- WDP-FE-EXE/vite.config.ts
+++ WDP-FE-EXE/vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'https://exe2-evservice.onrender.com/',
     },
     headers: {
       // ⚠️ Đây là phần bạn cần thêm
